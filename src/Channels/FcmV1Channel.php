668f81fffa651de309a56cb7ea8417caf010257d
Quan Vu <quan@Quans-MacBook-Pro.local>
2024-09-09 11:38:00 +0700

    fix notify

--- src/Channels/FcmV1Channel.php
+++ src/Channels/FcmV1Channel.php
@@ -2,6 +2,8 @@
 
 namespace Edujugon\PushNotification\Channels;
 
+use Edujugon\PushNotification\Messages\PushMessage;
+
 class FcmV1Channel extends GcmChannel
 {
     /**
@@ -10,5 +12,44 @@ class FcmV1Channel extends GcmChannel
     protected function pushServiceName()
     {
         return 'fcmv1';
+    }
+    protected function buildData(PushMessage $message)
+    {
+        $data = [];
+        if ($message->title != null || $message->body != null || $message->click_action != null) {
+            $data = [
+                'notification' => [
+                    'title' => $message->title,
+                    'body' => $message->body,
+//                    'sound' => $message->sound,
+//                    'color' => $message->color,
+//                    'click_action' => $message->click_action,
+                ],
+//                'android'=>[]
+            ];
+
+            // Set custom badge number when isset in PushMessage
+            if (! empty($message->badge)) {
+                $data['notification']['badge'] = $message->badge;
+            }
+
+            // Set icon when isset in PushMessage
+            if (! empty($message->icon)) {
+                $data['notification']['icon'] = $message->icon;
+            }
+            if (! empty($message->click_action)) {
+                $data['android']= [
+                        "notification"=> [
+                        "click_action"=> $message->click_action
+                    ]
+                ];
+            }
+        }
+
+        if (! empty($message->extra)) {
+            $data['data'] = $message->extra;
+        }
+
+        return $data;
     }
 }
